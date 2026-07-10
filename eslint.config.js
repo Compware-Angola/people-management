250aa292d8530649a9bdfba4d8dafa5ba44ec62d
Domingos Canhanga <canhanga96@gmail.com>
2026-07-10 12:05:20 +0100

    feat: integrate tanstack query with react-router and add related eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,9 @@
 //  @ts-check
 
 import { tanstackConfig } from '@tanstack/eslint-config'
-
+import pluginQuery from '@tanstack/eslint-plugin-query'
 export default [
+  ...pluginQuery.configs['flat/recommended'],
   ...tanstackConfig,
   {
     rules: {
